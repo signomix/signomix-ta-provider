7f3cdc7ad5bf47cd53124542579a84202fb58fb2
Grzegorz Skorupa <g.skorupa@gmail.com>
2023-02-26 22:10:48 +0100

    wip

--- src/main/java/com/signomix/provider/ProviderService.java
+++ src/main/java/com/signomix/provider/ProviderService.java
@@ -1,7 +1,12 @@
 package com.signomix.provider;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
@@ -12,9 +17,12 @@ import org.jboss.logging.Logger;
 
 import com.signomix.common.db.AuthDao;
 import com.signomix.common.db.AuthDaoIface;
+import com.signomix.common.db.DataQuery;
+import com.signomix.common.db.DataQueryException;
 import com.signomix.common.db.IotDatabaseDao;
 import com.signomix.common.db.IotDatabaseException;
 import com.signomix.common.db.IotDatabaseIface;
+import com.signomix.common.iot.ChannelData;
 
 import io.agroal.api.AgroalDataSource;
 import io.quarkus.agroal.DataSource;
@@ -48,24 +56,24 @@ public class ProviderService {
     public void onApplicationStart(@Observes StartupEvent event) {
         dataDao = new IotDatabaseDao();
         dataDao.setDatasource(ds);
-        authDao=new AuthDao();
+        authDao = new AuthDao();
         authDao.setDatasource(ds2);
-        try{
-            LOG.info("requestLimitConfigured:"+requestLimitConfigured);
-            int requestLimit=Integer.parseInt(requestLimitConfigured);
+        try {
+            LOG.info("requestLimitConfigured:" + requestLimitConfigured);
+            int requestLimit = Integer.parseInt(requestLimitConfigured);
             dataDao.setQueryResultsLimit(requestLimit);
-        }catch(Exception e){
+        } catch (Exception e) {
             LOG.error(e.getMessage());
         }
     }
 
-    @CacheResult(cacheName = "token-cache") 
+    @CacheResult(cacheName = "token-cache")
     String getUserID(String sessionToken) {
         LOG.debug("token:" + sessionToken);
         return authDao.getUser(sessionToken);
     }
 
-    @CacheResult(cacheName = "query-cache") 
+    @CacheResult(cacheName = "query-cache")
     List getData(String userID, String deviceEUI, String channelName, String query) {
         LOG.debug("userID:" + userID);
         LOG.debug("device:" + deviceEUI);
@@ -75,9 +83,9 @@ public class ProviderService {
             if (channelName != null && !"$".equals(channelName)) {
                 ArrayList result = (ArrayList) dataDao.getValues(userID, deviceEUI,
                         query + " channel " + channelName);
-                return result;
+                return normalize(result, query + " channel " + channelName);
             } else {
-                return (ArrayList) dataDao.getValues(userID, deviceEUI, query);
+                return normalize((ArrayList) dataDao.getValues(userID, deviceEUI, query), query);
             }
         } catch (IotDatabaseException ex) {
             LOG.warn(ex.getMessage());
@@ -90,23 +98,100 @@ public class ProviderService {
         LOG.debug("group:" + groupEUI);
         LOG.debug("channel:" + channelNames);
 
-        long organizationId=-1;
-        long secondsBack=3600;
-        //LOG.debug("query:" + query);
+        long organizationId = -1;
+        long secondsBack = 3600;
+        // LOG.debug("query:" + query);
         try {
-            //if (channelName != null && !"$".equals(channelName)) {
-            //    String query2=null!=query?query:"";
-            //    query2=query2+" channel " + channelName;
-            //    ArrayList result = (ArrayList) dataDao.getGroupValues(userID, groupEUI,query2);
-            //    return new ArrayList<>();
-            //} else {
-                //return (ArrayList) dataDao.getGroupValues(userID, groupEUI, channelNames);
-                return dataDao.getValuesOfGroup(userID, organizationId, groupEUI, channelNames, secondsBack);
-            //}
+            // if (channelName != null && !"$".equals(channelName)) {
+            // String query2=null!=query?query:"";
+            // query2=query2+" channel " + channelName;
+            // ArrayList result = (ArrayList) dataDao.getGroupValues(userID,
+            // groupEUI,query2);
+            // return new ArrayList<>();
+            // } else {
+            // return (ArrayList) dataDao.getGroupValues(userID, groupEUI, channelNames);
+            return dataDao.getValuesOfGroup(userID, organizationId, groupEUI, channelNames, secondsBack);
+            // }
         } catch (IotDatabaseException ex) {
             LOG.warn(ex.getMessage());
             return new ArrayList();
         }
 
+    }
+
+    /**
+     * Fills missing measurements with null values.
+     * 
+     * @param data
+     * @return
+     */
+    private ArrayList normalize(ArrayList data, String query) {
+        DataQuery dq;
+        try{
+            dq=DataQuery.parse(query);
+        }catch(DataQueryException ex){
+            ex.printStackTrace();
+            return data;
+        }
+        List<String> channelNames=dq.getChannels();
+        HashMap<String, SortedMap<Long, ChannelData>> map = new HashMap<>();
+        ArrayList<ArrayList> result=new ArrayList<>();
+        ArrayList<ChannelData> subList;
+        ChannelData tmpCd;
+        int numberOfLists = data.size();
+        if (channelNames.size() < 2) {
+            return data;
+        }
+        SortedMap<Long, ChannelData> subMap;
+        for (int i = 0; i < numberOfLists; i++) {
+            LOG.info("i=="+i);
+            subList = (ArrayList<ChannelData>) data.get(i);
+            for (int j = 0; j < subList.size(); j++) {
+                LOG.info("j=="+j);
+                tmpCd = subList.get(j);
+                subMap=map.get(tmpCd.getName());
+                if ( null==subMap) {
+                    subMap=new TreeMap<>();
+                }
+                subMap.put(tmpCd.getTimestamp(), tmpCd);
+                map.put(tmpCd.getName(), subMap);
+            }
+        }
+
+        String mainKey, key;
+        Iterator<Long> it;
+        SortedMap<Long, ChannelData> submap2, tmpMap;
+        Long timestamp;
+        for (int i = 0; i < channelNames.size(); i++) {
+            mainKey = (String) channelNames.get(i);
+            submap2 = map.get(mainKey);
+            it = submap2.keySet().iterator();
+            while (it.hasNext()) {
+                timestamp=it.next();
+                for (int j = 0; j < channelNames.size(); j++) {
+                    key = (String) channelNames.get(j);
+                    if (!key.equals(mainKey)) {
+                        tmpMap = map.get(key);
+                        if(null==tmpMap.get(timestamp)){
+                            tmpCd=submap2.get(timestamp);
+                            tmpMap.put(timestamp,new ChannelData(tmpCd.getDeviceEUI(),key,null,timestamp));
+                        }
+                    }
+                }
+            }
+        }
+        ArrayList<Long> timestamps=new ArrayList<>();
+        for (Map.Entry mapElement : map.get(channelNames.get(0)).entrySet()) {
+            timestamps.add( (Long)mapElement.getKey() );
+        }
+        for(int i=0; i<timestamps.size(); i++){
+            subList=new ArrayList<>();
+            for(int j=0; j<channelNames.size(); j++){
+                subList.add(map.get(channelNames.get(j)).get(timestamps.get(i)));
+            }
+            result.add(subList);
+        }
+
+        return result;
     }
 }
